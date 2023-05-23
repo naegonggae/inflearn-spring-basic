530092504bbaf5009b06a6610731f390943f697c
naegonggae <tkdtkd97@naver.com>
2023-05-23 18:57:39 +0900

    :sparkles: 회원 도메인 실행과 테스트

---
+++ src/main/java/hello/core/MemberApp.java
@@ -0,0 +1,23 @@
+package hello.core;
+
+import hello.core.member.Grade;
+import hello.core.member.Member;
+import hello.core.member.MemberService;
+import hello.core.member.MemberServiceImpl;
+
+public class MemberApp {
+
+	// 순수 자바로 테스트한것임 스프링은 사용되지 않았다.
+	// 이렇게 테스트하는것은 효율적인 방법이 아니니 테스트 코드를 작성하자
+	public static void main(String[] args) {
+		MemberService memberService = new MemberServiceImpl();
+		Member member = new Member(1L, "Member1", Grade.VIP);
+		memberService.join(member);
+
+		Member findMember = memberService.findMember(1L);
+
+		System.out.println("new member = " + member.getName());
+		System.out.println("find Member = " + findMember.getName());
+	}
+
+}
--- src/main/java/hello/core/member/MemberServiceImpl.java
+++ src/main/java/hello/core/member/MemberServiceImpl.java
@@ -3,6 +3,9 @@ package hello.core.member;
 public class MemberServiceImpl implements MemberService {
 
 	// 아직 의존을 객체 생성해서 받아야한다.
+	// Solid 의 원칙중에 DIP 를 잘 지키고 있는가?
+	// 실제로 MemberServiceImpl 는 MemberRepository 라는 역할만을 의존하는게 아니라 역할 + MemoryMemberRepository 구현체도 의존하고 있어 위반되고 있다.
+	// 즉, 추상화에도 의존하고 구체화에도 의존을 하고 있다.
 	private final MemberRepository memberRepository = new MemoryMemberRepository();
 
 	@Override
